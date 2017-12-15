ebc2a247b2b3253bbd83f1dfb52fcf0e1c44c708
Nikolai Nowolodski <nikolai.nowolodski@AD.ITS-STUTTGART.LOGODIDACT.NET>
2017-12-15 11:44:18 +0100

    [Contact-form] done some shit

--- php/kontakt.php
+++ php/kontakt.php
@@ -15,25 +15,34 @@
     <?php include 'header.php' ?>
 
     <div class="container">
-		<form action="http://formspree.io/foodmen-group@inboxbear.com" method="post">
-		  <div class="form-group">
-			<label for="name">Dein Name:</label>
-			<input type="name" class="form-control" name="_next" id="name" placeholder="Horst? Max? Eva? Wie heißt du denn nun?!">
-			<label for="email">Deine Email Adresse:</label>
-			<input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="deinname@deinedomain.de">
-			<small id="emailHelp" class="form-text text-muted">Wir werden deine Email NIEMALS veröffentlichen oder teilen</small>
-		  </div>
-		  <div class="form-group">
-			<label for="betreff">Betreff:</label>
-			<input type="text" class="form-control" id="betreff" placeholder="Was willsch?">
-		  </div>
-		  <div class="form-group">
-			<label for="nachricht">Nachricht:</label>
-			<textarea class="form-control" id="nachricht" rows="3" placeholder="Erzähle uns mehr von deinem Bedürfniss"></textarea>
-		  </div>
-		  <button type="submit" name="senden" class="btn btn-primary">Nachricht senden</button>
-		</form>
-		</div>
+      <div>
+        <h1>Kontaktformular <i class="fas fa-envelope" style="color:#007bff"></i></h1>
+        <br>
+        <p>Für Fragen, Vorschläge oder Beschwerden nutzen Sie bitte das untenstehende Kontaktformular. Wir kümmern uns so schnell wie möglich um die Bearbeitung Ihrer Anfrage. Bei dringenden Angelegenheiten können Sie auch direkt unsere Angestellten ansprechen.</p>
+        <br>
+      </div>
+      <div class="contact-box">
+      <form class="" role="form" action="http://formspree.io/foodmen-group@inboxbear.com" method="post">
+        <input type="hidden" name="_next" value="mensa-projekt/php/success.html" />
+        <div class="form-group">
+     <label for="name">Dein Name:</label>
+     <input type="name" class="form-control" id="name" placeholder="Horst? Max? Eva? Wie heißt du denn nun?!">
+     <label for="email">Deine Email Adresse:</label>
+     <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="deinname@deinedomain.de">
+     <small id="emailHelp" class="form-text text-muted">Wir werden deine Email NIEMALS veröffentlichen oder teilen</small>
+     </div>
+     <div class="form-group">
+     <label for="betreff">Betreff:</label>
+     <input type="text" class="form-control" id="betreff" placeholder="Was willsch?">
+     </div>
+     <div class="form-group">
+     <label for="nachricht">Nachricht:</label>
+     <textarea class="form-control" id="nachricht" rows="3" placeholder="Erzähle uns mehr von deinem Bedürfniss"></textarea>
+     </div>
+     <button type="submit" name="senden" class="btn btn-primary">Nachricht senden</button>
+      </form>
+    </div>
+    </div>
 
     <?php include 'footer.php' ?>
   </body>
