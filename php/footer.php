62b73e2ddf19b3cc02dccb2c7290bd47a0833b98
NakedJavascript <nikolai.nowolodski@googlemail.com>
2017-12-23 16:41:07 +0100

    [Cookies] Fixed cookies

--- php/footer.php
+++ php/footer.php
@@ -2,10 +2,25 @@
 	include_once 'dependencies.php';
 ?>
 
-</div> <!--Ende vom PagecontentWrapper(siehe Header)-->
 	<footer class="footer">
-      <div class="container" style="margin-top:1%;">
+      <div class="container">
         <a href='kontakt.php'>Kontakt</a> | <a href='impressum.php'>Impressum</a> | <a href='datenschutz.php'>Datenschutz</a>
 	  	</div>
   </footer>
-<?php echo $footer_dependencies; ?>
+
+	<?php echo $footer_dependencies; ?>
+
+	<!-- Start Cookie Plugin -->
+	<script type="text/javascript">
+		window.cookieconsent_options = {
+		  message: 'Diese Website nutzt Cookies, um bestmögliche Funktionalität bieten zu können.',
+		  dismiss: 'Verstanden',
+		  learnMore: 'Mehr Infos',
+		  link: 'datenschutz.php',
+		  theme: 'dark-bottom'
+		};
+	</script>
+	<script type="text/javascript" src="../vendor/valao-cloud/script.js"></script>
+	<!-- Ende Cookie Plugin -->
+
+</div> <!--Ende vom PagecontentWrapper(siehe Header)-->
--- php/header.php
+++ php/header.php
@@ -24,70 +24,55 @@
 	}
 ?>
 
+<div class="pageContentWrapper">
+	<nav class="navbar navbar-expand-lg navbar-light bg-custom">
+		<a class="navbar-brand" href="index.php"><img src='../images/logo.png' width="120px"/></a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+		<span class="navbar-toggler-icon"></span>
+		</button>
 
-<nav class="navbar navbar-expand-lg navbar-light bg-custom">
-	<a class="navbar-brand" href="index.php"><img src='../images/logo.png' width="120px"/></a>
-	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-	<span class="navbar-toggler-icon"></span>
-	</button>
+		<div class="collapse navbar-collapse" id="navbarSupportedContent">
+			<ul class="navbar-nav mr-auto">
+				<li class="nav-item">
+					<a class="nav-link" href="benutzerliste.php">Benutzerliste</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="essensliste.php">Essensliste</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="profil.php">Profil</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="umsatz.php">Umsatz</a>
+				</li>
+			</ul>
+			<ul class="nav navbar-nav navbar-right">
+				<button type='button' class="btn btn-success" data-toggle="modal" data-target="#popUpWindow">Login</button>
 
-	<div class="collapse navbar-collapse" id="navbarSupportedContent">
-		<ul class="navbar-nav mr-auto">
-			<li class="nav-item">
-				<a class="nav-link" href="benutzerliste.php">Benutzerliste</a>
-			</li>
-			<li class="nav-item">
-				<a class="nav-link" href="essensliste.php">Essensliste</a>
-			</li>
-			<li class="nav-item">
-				<a class="nav-link" href="profil.php">Profil</a>
-			</li>
-			<li class="nav-item">
-				<a class="nav-link" href="umsatz.php">Umsatz</a>
-			</li>
-		</ul>
-		<ul class="nav navbar-nav navbar-right">
-			<button type='button' class="btn btn-success" data-toggle="modal" data-target="#popUpWindow">Login</button>
-
-			<div class="modal fade" id="popUpWindow">
-				<div class="modal-dialog">
-					<div class="modal-content">
-						<!-- header -->
-						<div class="modal-header">
-							<h3 class="modal-title">Login Form</h3>
-							<button type="button" class="close" data-dismiss="modal">&times;</button>
-						</div>
-						<!-- body -->
-						<div class="modal-body">
-							<form role="form" method="POST" enctype="multipart/form-data" action="<?php echo $_SERVER['PHP_SELF']?>">
-								<div class="form-group">
-									<label for="email">Email</label><input type="email" name="email" class="form-control"  placeholder="Email"/>
-									<label for="password" >Password</label><input type="password" name="passwort" class="form-control" placeholder="Passwort" />
-								</div>
-								<!-- footer -->
-								<div class="modal-footer">
-									<input type="submit" name="submit" class="btn btn-primary btn-block" value="Einloggen">
-								</div>
-							</form>
+				<div class="modal fade" id="popUpWindow">
+					<div class="modal-dialog">
+						<div class="modal-content">
+							<!-- header -->
+							<div class="modal-header">
+								<h3 class="modal-title">Login Form</h3>
+								<button type="button" class="close" data-dismiss="modal">&times;</button>
+							</div>
+							<!-- body -->
+							<div class="modal-body">
+								<form role="form" method="POST" enctype="multipart/form-data" action="<?php echo $_SERVER['PHP_SELF']?>">
+									<div class="form-group">
+										<label for="email">Email</label><input type="email" name="email" class="form-control"  placeholder="Email"/>
+										<label for="password" >Password</label><input type="password" name="passwort" class="form-control" placeholder="Passwort" />
+									</div>
+									<!-- footer -->
+									<div class="modal-footer">
+										<input type="submit" name="submit" class="btn btn-primary btn-block" value="Einloggen">
+									</div>
+								</form>
+							</div>
 						</div>
 					</div>
 				</div>
-			</div>
-		</ul>
-	</div>
-</nav>
-
-<!-- Start Cookie Plugin -->
-<script type="text/javascript">
-	window.cookieconsent_options = {
-	  message: 'Diese Website nutzt Cookies, um bestmögliche Funktionalität bieten zu können.',
-	  dismiss: 'Verstanden',
-	  learnMore: 'Mehr Infos',
-	  link: 'datenschutz.php',
-	  theme: 'dark-bottom'
-	};
-</script>
-<script type="text/javascript" src="../vendor/valao-cloud/script.js"></script>
-<!-- Ende Cookie Plugin -->
-
-<div class="pageContentWrapper">
+			</ul>
+		</div>
+	</nav>
