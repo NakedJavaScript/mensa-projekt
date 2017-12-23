f69a37b3793537641d8bffa3ee8833a17ba656ed
NakedJavascript <nikolai.nowolodski@googlemail.com>
2017-12-23 19:29:04 +0100

    [cookie] Changed complete style, fuck footer cookies

--- php/footer.php
+++ php/footer.php
@@ -1,34 +1,27 @@
 <?php
 	include_once 'dependencies.php';
 ?>
-	<div class="container-fluid test">
-		<div class="row">
-			<div class="col-sm-12" id="cookieCon"></div>
-		</div>
-		<div class="row">
-			<div class="col-sm-12">
-				<footer class="footer">
-			      <div class="container">
-			        <a href='kontakt.php'>Kontakt</a> | <a href='impressum.php'>Impressum</a> | <a href='datenschutz.php'>Datenschutz</a>
-				  	</div>
-			  </footer>
-			</div>
-		</div>
-	</div>
+
+	<footer class="footer">
+      <div class="container">
+        <a href='kontakt.php'>Kontakt</a> | <a href='impressum.php'>Impressum</a> | <a href='datenschutz.php'>Datenschutz</a>
+	  	</div>
+  </footer>
 
 	<?php echo $footer_dependencies; ?>
 
 	<!-- Start Cookie Plugin -->
+	<!-- src: http://website-tutor.com/cookie-plugin-script/ -->
 	<script type="text/javascript">
 		window.cookieconsent_options = {
-		  message: 'Diese Website nutzt Cookies, um bestmögliche Funktionalität bieten zu können.',
-		  dismiss: 'Verstanden',
-		  learnMore: 'Mehr Infos',
-		  link: 'datenschutz.php',
-		  theme: 'dark-bottom'
+			message: 'Diese Website nutzt Cookies, um bestmögliche Funktionalität bieten zu können.',
+			dismiss: 'Verstanden',
+			learnMore: 'Mehr Infos',
+			link: 'datenschutz.php',
+			theme: 'light-floating'
 		};
 	</script>
-	<script type="text/javascript" src="../vendor/valao-cloud/script.js"></script>
+	<script type="text/javascript" src="//s3.amazonaws.com/valao-cloud/cookie-hinweis/script.js"></script>
 	<!-- Ende Cookie Plugin -->
 
 </div> <!--Ende vom PagecontentWrapper(siehe Header)-->
