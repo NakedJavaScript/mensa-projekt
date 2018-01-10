ae56265ac9b562996255c45a2ff8cbc4493caed8
julian <34480385+WeZyBugFix@users.noreply.github.com>
2018-01-10 10:42:39 +0100

    Placed code for index.php into index_func.php
    Placed code for index.php into index_func.php

--- functions/index_func.php
+++ functions/index_func.php
@@ -1,3 +1,15 @@
 <?PHP 
-
+	//Code um eine Speise hinzuzufügen
+		if (isset($_POST['Tagesangebot_erstellen'])) {
+			$s_ID =$_POST['foodlist'];
+			$datum =strtotime($_POST['date']);
+			$formated= date('Y-m-d',$datum);
+			$insert = "INSERT INTO tagesangebot (speise_ID,datum)
+					VALUES ('$s_ID','$formated')";
+			if ($conn->query($insert) === TRUE) {
+				$Alert = successMessage("Tagesangebot wurde erfolgreich hinzugefügt");
+			} else {
+				$Alert = dangerMessage("Tagesangebot konnte nicht hinzugefügt werden");
+			}
+		}
 ?>
