f9d7d4cf9c2947a8a2543bb9fc4209db3319dbd8
julian <34480385+WeZyBugFix@users.noreply.github.com>
2018-01-24 10:59:23 +0100

    Some fine-tuning
    - Changed the name of some variables to something "more logical"
    - Added Comments
    - Moved the likeButton function to index_func.

--- php/functions/index_func.php
+++ php/functions/index_func.php
@@ -6,12 +6,43 @@
 			$formated= date('Y-m-d',$datum);
 			$insert = "INSERT INTO tagesangebot (speise_ID,datum)
 					VALUES ('$s_ID','$formated')";
-			if ($conn->query($insert) === TRUE) {
+			if ($conn->query($insert) === TRUE) { //Wenn Tagesangebot hinzugefügt wurde.
 				$Alert = successMessage("Tagesangebot wurde erfolgreich hinzugefügt");
-			} else {
-				$Alert = dangerMessage("Tagesangebot konnte nicht hinzugefügt werden");
+			} else { //Fall es nicht klappt wird der Nutzer mit einem Errorcode und einer Errornummer konfrontiert
+				$Alert = dangerMessage("<strong>Error:</strong>".$conn->errno.": ".$conn->error);
 			}
 		}
+		
+		//Funktion zum erstellen eines Like-Buttons
+		function likeButtons($foodID, $foodLikes, $has_liked){
+
+			if (isset($_SESSION['email'])) {
+						if ($_SESSION['adminrechte'] == 2 ) { //Falls ein Admin eingeloggt ist, kann dieser den Like-Button nicht anklicken.
+							return '<div class="like-container"><button type="button" class="btn heart-btn disabled" data-toggle="tooltip" data-placement="bottom" title="Als Administrator können Sie das Essen nicht liken!">
+								<i class="fas fa-heart like-heart-disabled"></i></button><p class="like-count">+'.$foodLikes.'</p></div>';
+
+						}
+								else if ($has_liked) {
+									return '<form role="form" method="POST" action="">
+												<input type="hidden" name="food_ID" value="'.$foodID.'">
+												<div class="like-container"><button type="submit" name="Speisen_unliken" class="btn heart-btn like-btn unlike" data-toggle="tooltip" data-placement="bottom" title="Diese Speise nicht mehr favorisieren">
+													<i class="fas fa-heart like-heart"></i></button><p class="like-count">+'.$foodLikes.'</p></div>
+												</form>';
+								}
+										else {
+											return '<form role="form" method="POST" action="">
+														<input type="hidden" name="food_ID" value="'.$foodID.'">
+														<div class="like-container"><button type="submit" name="Speisen_liken" class="btn heart-btn like-btn like"data-toggle="tooltip" data-placement="bottom" title="Diese Speise liken.">
+															<i class="fas fa-heart like-heart"></i></button><p class="like-count">+'.$foodLikes.'</p></div>
+														</form>';
+										}
+
+									}
+											else {
+												return '<div class="like-container"><button type="button" class="btn heart-btn disabled" data-toggle="tooltip" data-placement="bottom" title="Sie müssen eingeloggt sein um zu liken.">
+																<i class="fas fa-heart like-heart-disabled"></i></button><p class="like-count">+'.$foodLikes.'</p></div>';
+										}
+			}
 
 		// Code um Speisen zu liken
 		if (isset($_POST['Speisen_liken'])) {
