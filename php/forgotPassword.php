3e2b7e9cd6f86839fffd2778151d5897fa3dac9c
NakedJavascript <nikolai.nowolodski@googlemail.com>
2018-01-29 19:54:21 +0100

    fixed some code and outsourced code into new files

--- php/forgotPassword.php
+++ php/forgotPassword.php
@@ -1,5 +1,4 @@
-<?php include_once 'dependencies.php';
-?>
+<?php include_once 'dependencies.php'; ?>
 <!DOCTYPE HTML>
 <html>
     <head>
@@ -10,62 +9,27 @@
     </head>
 
     <body>
-            <?php include 'header.php';
-                if(isset($_SESSION['email'])) {
-                    include'footer.php';
-                    die('Sieht so aus als wären Sie eingeloggt, da können Sie ihr Passwort doch nicht vergessen haben!');
-                } //Verweigert nicht Admins den Zugriff auf diese Seite
+        <?php
+            include 'header.php';
+            include 'functions\forgotPW_function.php';
+        ?>
 
-                if (isset($_POST["forgotPass"])) {
-                    $email = $conn->real_escape_string($_POST["email"]);
-                    $data = $conn->query("SELECT benutzer_ID FROM benutzer WHERE email='$email'");
-
-                    if ($data->num_rows > 0) {
-                        $pepper = 'mensa_pfeffer';
-                        $options = array("cost"=>12);
-                        $str = "0123456789qwertzuiopasdfghjklyxcvbnmQWERTZUIOPASDFGHJKLYXCVBNM";
-                        $str = str_shuffle($str);
-                        $str = substr($str, 0,10);
-                        $url = "http://localhost/mensa-projekt/php/resetPassword.php?token=$str&email=$email";
-                        $mail->Subject = "Passwort zuruecksetzen";
-                        $mail->Body = "Um Ihr Passwort zurueckzusetzen, besuchen Sie bitte diese <a href='$url'>Seite</a>";
-                        $mail->addAddress($email);
-                        $mail->setFrom('foodmengroup@gmail.com', 'Foodmengroup');
-                        $mail->send();
-
-                        // Passwort wird gehashed
-                        $hashedPassword = password_hash($str . $pepper, PASSWORD_BCRYPT, $options);
-                        // Passwort wird verändert
-                        $conn->query("UPDATE benutzer SET passwort='$hashedPassword', token='$str' WHERE email='$email'");
-
-                        $Alert = 'Sie haben eine E-Mail von uns erhalten.';
-
-                        echo "<div class='alert alert-success alert-dismissable fade show' style='width:500px;'>
-                    					<a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>".$Alert."</div>";
-                    } else {
-                        $Alert = "Diese E-Mail existiert nicht!";
-                        echo "<div class='alert alert-danger alert-dismissable fade show' style='width:500px;'>
-                      <a href='essensliste.php' class='close' data-dismiss='alert' aria-label='close'>&times;</a>".$Alert."</div>";
-                    }
-                }
-            ?>
-
-            <div class="container flat-form">
-                <div id="reset" class="form-action">
-                    <h1>Passwort zurücksetzen</h1>
-                    <small>Um Ihr Passwort zurückzusetzen geben Sie bitte Ihre E-Mail Adresse im untenstehenden Feld ein. Sofern Ihre Adresse in unserem System registriert ist, erhalten Sie eine E-Mail mit einem Link um ein neues Passwort zu setzen.</small>
-                    <form action="forgotPassword.php" method="post" class="forgotPasswordForm">
-                        <ul>
-                            <li>
-                                <input type="email" name="email" placeholder="Ihre E-Mail" pattern="[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$" required>
-                            </li>
-                            <li>
-                                <input type="submit" class="btn btn-success" name="forgotPass" value="Passwort zurücksetzen">
-                            </li>
-                        </ul>
-                    </form>
-                </div>
+        <div class="container flat-form">
+            <div id="reset" class="form-action">
+                <h1>Passwort zurücksetzen</h1>
+                <small>Um Ihr Passwort zurückzusetzen geben Sie bitte Ihre E-Mail Adresse im untenstehenden Feld ein. Sofern Ihre Adresse in unserem System registriert ist, erhalten Sie eine E-Mail mit einem Link um ein neues Passwort zu setzen.</small>
+                <form action="forgotPassword.php" method="post" class="forgotPasswordForm">
+                    <ul>
+                        <li>
+                            <input type="email" name="email" placeholder="Ihre E-Mail" pattern="[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$" required>
+                        </li>
+                        <li>
+                            <input type="submit" class="btn btn-success" name="forgotPass" value="Passwort zurücksetzen">
+                        </li>
+                    </ul>
+                </form>
             </div>
+        </div>
     </body>
     <?php include 'footer.php'; ?>
 </html>
--- php/resetPassword.php
+++ php/resetPassword.php
@@ -10,67 +10,36 @@
     </head>
 
     <body>
-        <?php include 'header.php';
-
-        if(isset($_GET["email"]) && isset($_GET["token"])) {
-            $email = $conn->real_escape_string($_GET["email"]);
-            $token = $conn->real_escape_string($_GET["token"]);
-            $data = $conn->query("SELECT benutzer_ID FROM mensa.benutzer WHERE email='$email' AND token='$token'");
+        <?php
+            include 'header.php';
+            include 'functions/resetPW_function.php';
 
             if ($data->num_rows > 0) {
-                $pepper = 'mensa_pfeffer';
-                $options = array("cost"=>12);
                 echo "<div class='container flat-form'>
-                        <div id='reset' class='form-action'>
-                            <h1>Neues Passwort setzen</h1>
-                            <small>Geben Sie hier bitte Ihr neues Passwort ein.</small>
-                            <form role='form' method='POST' class='resetPasswordForm'>
-                                <ul>
-                                    <li>
-                                        <input type='password' name='neuesPasswort' class='form-control' required/>
-                                    </li>
-                                    <li>
-                                        <input type='submit' name='submitNewPassword' class='btn btn-success'>
-                                    </li>
-                                </ul>
-                            </form>
-                        </div>
-                     </div>";
-
-                if ( isset( $_POST['submitNewPassword'] ) ) {
-                    $newPassword = $_POST['neuesPasswort'];
-                    // Passwort wird gehashed
-                    $hashedPassword = password_hash($newPassword . $pepper, PASSWORD_BCRYPT, $options);
-                    // Passwort wird verändert, token wird wieder auf empty gesetzt
-                    $conn->query("UPDATE benutzer SET passwort='$hashedPassword', token='' WHERE email='$email'");
-
-                    $Alert = 'Dein Passwort wurde erfolgreich geändert!';
-
-                    echo "<div class='alert alert-success alert-dismissable fade show' style='width:500px;'>
-                                    <a href='#' class='close' data-dismiss='alert' aria-label='close'>&times;</a>".$Alert."</div>
-
-                  <script>setTimeout(function () {
-                     window.location.href= 'index.php';
-
-                 },3000);</script>";
-                }
+                    <div id='reset' class='form-action'>
+                        <h1>Neues Passwort setzen</h1>
+                        <small>Geben Sie hier bitte Ihr neues Passwort ein.</small>
+                        <form role='form' method='POST' class='resetPasswordForm'>
+                            <ul>
+                                <li>
+                                    <input type='password' name='neuesPasswort' class='form-control' required/>
+                                </li>
+                                <li>
+                                    <input type='submit' name='submitNewPassword' class='btn btn-success'>
+                                </li>
+                            </ul>
+                        </form>
+                    </div>
+                 </div>";
             } else {
-                $Alert = "Der aufgerufene Link ist abgelaufen oder falsch!";
-                echo "<div class='alert alert-danger alert-dismissable fade show''>
-              <a href='essensliste.php' class='close' data-dismiss='alert' aria-label='close'>&times;</a>".$Alert."</div>
+                $Alert = dangerMessage("Der aufgerufene Link ist abgelaufen oder falsch!");
 
-              <script>setTimeout(function () {
-                 window.location.href= 'index.php';
-
-             },2000);</script>";
+                echo $Alert . " <div class='container' id='error-img-wrapper'><img src='../images/taco-error.png'></div>
+                <script>setTimeout(function () {
+                    window.location.href= 'index.php';
+                },3000);</script>";
             }
-
-        } else {
-            header("Location: index.php");
-            exit();
-        }
-
-?>
-</body>
+        ?>
+    </body>
 <?php include 'footer.php'; ?>
 </html>
