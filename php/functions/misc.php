c5f6dced12f2a531da7923e15f37045dc880ce5f
Orangenspannung <andreas.hruschka1@gmail.com>
2018-02-07 19:29:29 +0100

    delete echos
    Related to:M64

--- php/functions/misc.php
+++ php/functions/misc.php
@@ -5,7 +5,6 @@ function validate($field, $reserved_keywords) {
     if (count($field_parts)>1) {
         if (!preg_match('/[\'^£$%&*()}{@#~?><>,|=_+¬-]/', $field)) {
             foreach ($field_parts as $part) {
-                echo $part;
                 if (in_array($part, $reserved_keywords)) {
                     $is_valid = false;
                     break;
@@ -24,9 +23,7 @@ function sanitize_form($form) {
                         'between', 'join', 'inner', 'left', 'right', 'full', 'self', 'union', 'group', 'having', 'exists', 'any', 'all', 'create', 'drop', 'alter', 'unique',
                          'primary', 'foreign', 'check', 'default' ,'index' ,'increment'];
     foreach($form as $key => $value) {
-        echo $value;
         $simple_field = strtolower(trim($value));
-        echo $simple_field;
         if (validate($simple_field, $reserved_keywords)) {
             $form[$key] = $value;
         } else {
